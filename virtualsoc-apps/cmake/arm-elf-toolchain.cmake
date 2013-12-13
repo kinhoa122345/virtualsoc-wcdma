f21f8b62d199d0c1d1347f7a1c303c62f41c920e
Franck Duriez <franck.lucien.duriez@gmail.com>
2013-12-13 14:13:30 +0100

    Update CMake scripts.

--- virtualsoc-apps/cmake/arm-elf-toolchain.cmake
+++ virtualsoc-apps/cmake/arm-elf-toolchain.cmake
@@ -1,12 +1,41 @@
 # This one is important
 set(CMAKE_SYSTEM_NAME Linux)
 
-#THis one not so much
+message(STATUS "Searching for arm-elf-gcc")
+
+# The HINTS option should only be used for values computed from the system.
+SET(_ARM_ELF_GCC_HINTS
+  ${ARM_ELF_GCC_PREFIX}
+  ${ARM_ELF_GCC_PREFIX}/bin
+  $ENV{ARM_ELF_GCC_PREFIX}
+  $ENV{ARM_ELF_GCC_PREFIX}/bin
+  ${CMAKE_INSTALL_PREFIX}
+  ${CMAKE_INSTALL_PREFIX}/bin
+  )
+
+# Hard-coded guesses should still go in PATHS. This ensures that the user
+# environment can always override hard guesses.
+SET(_ARM_ELF_GCC_PATHS
+  /opt/ensta/pack/virtualsoc/arm-elf-gcc-4.5.3
+  /usr/local/bin
+  /usr/bin
+  /bin
+  )
+
+# This one not so much
 set(CMAKE_SYSTEM_VERSION 1)
 
-# Specify the cross compiler
-set(CMAKE_C_COMPILER   "/opt/ensta/pack/virtualsoc/arm-elf-gcc-4.5.3/bin/arm-elf-gcc")
-set(CMAKE_CXX_COMPILER "/opt/ensta/pack/virtualsoc/arm-elf-gcc-4.5.3/bin/arm-elf-g++")
+FIND_FILE(CMAKE_C_COMPILER
+  NAMES arm-elf-gcc
+  HINTS ${_SIMSOC_HINTS}
+  PATHS ${_SIMSOC_PATHS}
+)
+
+FIND_PATH(CMAKE_CXX_COMPILER
+  NAMES arm-elf-g++
+  HINTS ${_SIMSOC_HINTS}
+  PATHS ${_SIMSOC_PATHS}
+)
 
 # Where is the target environment
 set(CMAKE_FIND_ROOT_PATH
